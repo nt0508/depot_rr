46f8d14b952e71fb155291aa5c067135237cab98
nt0508 <i0tl.96er@gmail.com>
2017-11-23 14:38:28 +0700

    Task I-3

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,5 +5,16 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  class ActionDispatch::IntegrationTest
+    def login_as(user)
+      post login_url, params: { name: user.name, password: 'secret' }
+    end
+    def logout
+      delete logout_url
+    end
+    def setup
+      login_as users(:one)
+    end
+  end
   # Add more helper methods to be used by all tests here...
 end
