67092cf08289ec594aff02113a909481d7c0aad2
nt0508 <i0tl.96er@gmail.com>
2017-11-23 15:12:01 +0700

    Task J

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :set_i18n_locale_from_params
   before_action :authorize
   protect_from_forgery with: :exception
 
@@ -10,4 +11,16 @@ class ApplicationController < ActionController::Base
       end
     end
 
+    def set_i18n_locale_from_params
+      if params[:locale]
+        if I18n.available_locales.map(&:to_s).include?(params[:locale])
+          I18n.locale = params[:locale]
+        else
+
+        flash.now[:notice] =
+          "#{params[:locale]} translation not available"
+        logger.error flash.now[:notice]
+      end
+    end
+  end
 end
